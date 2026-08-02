8f1e92b5b361b090046d2d1f96ab7cdf62684a1f
anushka <anushka28chourasia@gmail.com>
2026-08-02 19:55:14 +0530

    Initial Spring Boot setup

---
+++ src/main/java/fintrack_backend/FintrackBackendApplication.java
@@ -0,0 +1,13 @@
+package fintrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackBackendApplication.class, args);
+	}
+
+}
