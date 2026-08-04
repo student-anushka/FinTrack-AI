116af06d74d24f06a5a969aec4fc73a9eabe088e
anushka <anushka28chourasia@gmail.com>
2026-08-04 09:37:53 +0530

    feat: prevent duplicate email registration

--- src/main/java/com/fintrack/auth/repository/UserRepository.java
+++ src/main/java/com/fintrack/auth/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByEmail(String email);
+    // existsbyEmail : checks in db whether the email is there or not
+    // Spring Data JPA automatically implements it
 }
--- src/main/java/com/fintrack/auth/service/AuthService.java
+++ src/main/java/com/fintrack/auth/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.fintrack.auth.service;
 
 import com.fintrack.auth.dto.RegisterRequest;
+import com.fintrack.auth.dto.response.RegisterResponse;
 import com.fintrack.auth.entity.User;
 import com.fintrack.auth.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -18,19 +19,24 @@ public class AuthService {
 
     public RegisterResponse register(RegisterRequest request) {
 
-        User user = new User();
-
-        user.setFirstName(request.getFirstName());
-        user.setLastName(request.getLastName());
-        user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
-        user.setRole("USER");
-        user.setEmailVerified(false);
-        user.setCreatedAt(LocalDateTime.now());
-        user.setUpdatedAt(LocalDateTime.now());
-
-        userRepository.save(user);
-
-        return new RegisterResponse("User Registered Successfully!");
+    if (userRepository.existsByEmail(request.getEmail())) {
+        return new RegisterResponse("Email already exists!");
     }
+
+    User user = new User();
+
+    user.setFirstName(request.getFirstName());
+    user.setLastName(request.getLastName());
+    user.setEmail(request.getEmail());
+    user.setPassword(request.getPassword());
+    user.setRole("USER");
+    user.setEmailVerified(false);
+    user.setCreatedAt(LocalDateTime.now());
+    user.setUpdatedAt(LocalDateTime.now());
+
+    userRepository.save(user);
+
+    return new RegisterResponse("User Registered Successfully!");
+
+}
 }
